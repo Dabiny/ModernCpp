4dc4389a54961c20525931a93e6e0370407ca0f1
Dabiny <lina917@naver.com>
2022-04-07 18:33:26 +0900

    no message

---
+++ modernCpp/modernCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  modernCpp
+//
+//  Created by Dabin Kim on 2022/04/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
